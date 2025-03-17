05a9079f684b11e8cdd27a0b6fcc4faa2aa8faef
Biswarup Mahato <107302651+biswarupmahato7@users.noreply.github.com>
2025-03-17 17:50:09 +0530

    Data base Connection change

--- backend/Connection/mongoDb.js
+++ backend/Connection/mongoDb.js
@@ -1,22 +1,7 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 
-// Load environment variables
-
-dotenv.config();
-
-const connectToDatabase = async () => {
-    try {
-        await mongoose.connect(process.env.DB_URI, {
-            serverSelectionTimeoutMS: 5000,
-            socketTimeoutMS: 45000,
-            tls: true,
-            family: 4,
-        });
-        console.log("Connected to DB");
-    } catch (error) {
-        console.error("Error connecting to database", error);
-    }
-};
-
-export default connectToDatabase;
+mongoose
+  .connect('mongodb://localhost:27017/youtubeBackend')
+  .then(() => console.log('DB connection successful!')).catch(err=>{
+    console.log(err)
+  });
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import connectToDatabase from './Connection/mongoDb.js'
+// import connectToDatabase from './Connection/mongoDb.js'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 
 import { userRoutes } from './Routes/user.routes.js'
 import { videoRoutes } from './Routes/video.routes.js'
 import { commentRoutes } from './Routes/comment.routes.js'
+import './Connection/mongoDb.js'
 
 const app = express()
 const port = 4000
@@ -24,7 +25,7 @@ app.get('/', (req,res) =>{
 
 app.listen(port, async ()=>{
     console.log(`Server is Running at port no: ${port}`)
-    await connectToDatabase()
+   
 
 })
 
